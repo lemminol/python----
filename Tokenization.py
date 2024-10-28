6172ab94a933921e22a94cb01cd0ac49a67dd9ac
lemminol <30825512+lemminol@users.noreply.github.com>
2024-10-28 20:28:38 +0900

    new file:   Tokenization.py

---
+++ Tokenization.py
@@ -0,0 +1,5 @@
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import WordPunctTokenizer
+from tensorflow.keras.preprocessing.text import text_to_word_sequence
+
+print('단어 토큰화1 :',word_tokenize("Don't be fooled by the dark sounding name, Mr. Jone's Orphanage is as cheery as cheery goes for a pastry shop."))
